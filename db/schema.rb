07214ef067794a4f51ccc5ca245580ededd0aa46
Andrew Buckingham <abuckingham99@gmail.com>
2015-03-13 23:18:54 -0500

    Add foreign key to Posts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150313210159) do
+ActiveRecord::Schema.define(version: 20150314040911) do
 
   create_table "posts", force: true do |t|
-    t.string "url"
-    t.string "title"
-    t.text   "description"
+    t.string   "url"
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
