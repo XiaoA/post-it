0dc0d7754ad11491f99ef6f56cb4a050a7059fc6
Andrew Buckingham <abuckingham99@gmail.com>
2015-03-27 18:11:19 -0500

    Add 'update' method to posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,7 +28,15 @@ class PostsController < ApplicationController
   end
 
   def update
-     
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+    else
+      flash[:notice] = "The post was updated."
+      redirect_to post_path(@post)
+    else
+      render 'edit'
+    end
   end
 
   private
