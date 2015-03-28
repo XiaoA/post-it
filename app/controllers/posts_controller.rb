80410d0ee4537985d8fa57a7b99fc1f4ff0d245d
Andrew Buckingham <abuckingham99@gmail.com>
2015-03-27 21:04:07 -0500

    Add 'before_action' to post_controllers.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+                                 
   def index
     @post = Post.all
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+  def show; end
 
   def new
     @post = Post.new
@@ -23,13 +23,9 @@ class PostsController < ApplicationController
     end
   end
 
-  def edit
-    @post = Post.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @post = Post.find(params[:id])
-
     if @post.update(post_params)
       flash[:notice] = "The post was updated."
       redirect_to post_path(@post)
@@ -42,5 +38,9 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit!
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
