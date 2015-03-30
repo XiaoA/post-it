9a29b88110a0d57f187ffc69dba4bcc196a64ea9
Andrew Buckingham <abuckingham99@gmail.com>
2015-03-29 22:47:26 -0500

    Create nested comments resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
 
-  resources :posts, except: [:destroy]
-  resources :comments, only: [:create, :show]
+  resources :posts, except: [:destroy] do
+    resources :comments, only: [:create, :show]
+  end
   resources :categories, only: [:new, :create, :show]
 end
