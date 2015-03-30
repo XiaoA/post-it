21eaff500d898a2f7e530dcfe45ae47aa22a7dcf
Andrew Buckingham <abuckingham99@gmail.com>
2015-03-29 22:48:12 -0500

    Add comments_controller.rb

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(params.require(:comment).permit(:body))
+
+
+    if comment.save
+      flash[:notice] = "Your comment was added."
+      redirect_to post_path(@post)
+    else
+      render "post/show"
+    end
+  end
+
+  def comment_params
+    @comment.permit
+  end
+end
