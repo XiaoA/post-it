17cf966da006f35a80a5174cfae3293597aed39f
Andrew Buckingham <abuckingham99@gmail.com>
2015-03-29 23:01:15 -0500

    Fix typo in comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,7 +9,7 @@ class CommentsController < ApplicationController
       flash[:notice] = "Your comment was added."
       redirect_to post_path(@post)
     else
-      render "post/show"
+      render "posts/show"
     end
   end
 
