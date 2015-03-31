048f3ce7ffde161b6311a8072ebd4bfd76da57da
Andrew Buckingham <abuckingham99@gmail.com>
2015-03-30 23:13:26 -0500

    Add display_datetime ApplicationHelper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def fix_url(str)
     str.starts_with?("http://") ? str: "http://#{str}"
   end
+
+  def display_datetime(dt)
+    dt.strftime("%B, %-m, %Y at %l:%S:%M %Z")
+  end
 end
