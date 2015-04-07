fc5d356d415e70db0c116ad8f8d3424cf83180b0
XiaoA <abuckingham99@gmail.com>
2015-04-06 23:10:39 -0500

    Fix 'create' method typo, sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.where(username: params[:username]).first
-    if && user.authenticate(params[:password])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:notice] = "Welcome, you've logged in."
       redirect_to root_path
