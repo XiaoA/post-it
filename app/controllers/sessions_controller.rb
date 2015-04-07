385f38d7d5b2b0cd3324bd1823300e6f476d762a
XiaoA <abuckingham99@gmail.com>
2015-04-06 21:50:15 -0500

    Setup create method in SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    user = User.where(username: params[:username]).first
+    if && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Welcome, you've logged in."
+      redirect_to root_path
+    else
+      flash[:error] = "There is something wrong with your username or password."
+      redirect_to login_path
+    end
+                      
   end
 end
