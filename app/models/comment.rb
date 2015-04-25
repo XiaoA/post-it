182ffdaff80e3b9600567f37aafc9e614ffbf169
Andrew Buckingham <abuckingham99@gmail.com>
2015-04-24 23:42:43 -0500

    Update polymorphic association in comment.rb model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base 
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   belongs_to :post
-  has_many :votes, as: :voteable
+  has_many :votes, as: :polymorphic
 
   validates :body, presence: true
 end
