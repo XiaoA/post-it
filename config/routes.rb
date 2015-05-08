ae30baea8d276e2a8f3f3d8937c0441a7ad316c4
Andrew Buckingham <abuckingham99@gmail.com>
2015-05-08 00:03:19 -0500

    Add static 'About' page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ PostitTemplate::Application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+  get '/pin', to: 'sessions#pin'
+  post '/pin', to: 'sessions#pin'
+  get '/about', to: 'static#show'
   
   resources :posts, except: [:destroy] do
     member do
